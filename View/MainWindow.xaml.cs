b7d0ad00f719283e27b703d8d281aac5a1891bd3
hollowzen123 <eg.malyn.com@gmail.com>
2022-10-10 22:41:46 +0300

    Create architecture

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AutoSound
+namespace AutoSound.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
