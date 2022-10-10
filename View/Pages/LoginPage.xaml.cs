e5ce148809e2c725a171837812db099aea9a722a
hollowzen123 <eg.malyn.com@gmail.com>
2022-10-10 23:41:29 +0300

    Create UI LoginPage.xaml

---
+++ View/Pages/LoginPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AutoSound.View.Pages;
+
+public partial class LoginPage : Page
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+}
+
